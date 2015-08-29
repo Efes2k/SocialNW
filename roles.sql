7c823988caa9326070d7966e61a27319ea52c416
Andrei <efes4k@gmail.com>
2015-08-29 17:05:52 +0300

    Update roles.sql

--- roles.sql
+++ roles.sql
@@ -1,3 +1,2 @@
 INSERT INTO `role` (`roledesc`,`rolename`) VALUES ('Adminitrator','Administrator');
-INSERT INTO `role` (`roledesc`,`rolename`) VALUES ('Manager','Manager');
 INSERT INTO `role` (`roledesc`,`rolename`) VALUES ('Ukraine','Manager');
