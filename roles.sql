6d6e14f34c76d3afe5139c6f69002794ec904aff
Andrei <efes4k@gmail.com>
2015-08-29 16:26:32 +0300

    Create roles.sql

---
+++ roles.sql
@@ -0,0 +1,3 @@
+INSERT INTO `role` (`roledesc`,`rolename`) VALUES ('Adminitrator','Administrator');
+INSERT INTO `role` (`roledesc`,`rolename`) VALUES ('Manager','Manager');
+INSERT INTO `role` (`roledesc`,`rolename`) VALUES ('Ukraine','Manager');
