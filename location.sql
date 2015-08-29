1db110eaaab97242fba422f06543f380c507e5df
Andrei <efes4k@gmail.com>
2015-08-29 16:26:47 +0300

    Update location.sql

--- location.sql
+++ location.sql
@@ -1,17 +1,3 @@
-CREATE TABLE `Country` (  
-      `id` int(6) NOT NULL AUTO_INCREMENT,  
-	    `country_name` varchar(200) NOT NULL,  
-      PRIMARY KEY (`id`)  
-    ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;  
-	
-CREATE TABLE `city` (  
-      `id` int(6) NOT NULL AUTO_INCREMENT,  
-	    `city_name` varchar(200) NOT NULL,  
-  	  `country_id` int(6) NOT NULL,	  
-	  FOREIGN KEY (`country_id`) REFERENCES Country(id), 
-      PRIMARY KEY (`id`)  
-    ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;  
-	
 INSERT INTO `country` (`country_name`) VALUES ('Belarus');
 INSERT INTO `country` (`country_name`) VALUES ('Russia');
 INSERT INTO `country` (`country_name`) VALUES ('Ukraine');
